3a02916df2cda81d7720b4ccd4af29b8f6e09473
shiran-sandaruwan-69x <shiran101997@gmail.com>
2025-08-17 04:36:29 +0530

    implemented customer dao

--- src/main/java/lk/pahana_billing_system/dao/CustomerDAO.java
+++ src/main/java/lk/pahana_billing_system/dao/CustomerDAO.java
@@ -1,4 +1,12 @@
 package lk.pahana_billing_system.dao;
 
-public class CustomerDAO {
+import lk.pahana_billing_system.model.Customer;
+import java.util.List;
+public interface CustomerDAO {
+    boolean addCustomer(Customer customer);
+    Customer getCustomerById(String id);
+    List<Customer> getAllCustomers();
+    boolean updateCustomer(Customer customer);
+    boolean deleteCustomer(String id);
+    int getCustomerCount();
 }
